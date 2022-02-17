e1a8e4e942d07540569c818b8fe27eb0428ec873
Michael Navazhylau <mikipux7@gmail.com>
2022-02-17 17:28:47 -0600

    Got Robot dog to lift a leg

---
+++ SpotSim/Assets/SpotControllerScripts/SpotController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpotController : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Joint
+    {
+        public string inputAxis;
+        public GameObject robotPart;
+    }
+    public Joint[] joints;
+
+    private ArticulationJointController[] controllers;
+
+    void Start()
+    {
+        // Get all joints
+        controllers = new ArticulationJointController[joints.Length];
+        for (int i = 0; i < joints.Length; i++)
+        {
+            controllers[i] = joints[i].robotPart.GetComponent<ArticulationJointController>();
+        }
+
+        UpdateRotation(1, SpotState.Go, -10.0f);
+        UpdateRotation(2, SpotState.Go, -40.0f);
+        
+        
+        
+    }
+
+    // Stop the joints if necessary
+    public void StopAllJointRotations()
+    {
+        for (int i = 0; i < joints.Length; i++)
+        {
+            UpdateRotation(i, SpotState.Stop, 0f);
+        }
+    }
+
+    // Updates rotation goal for controller
+    public void UpdateRotation(int jointIndex, SpotState state, float jointPosition)
+    {
+        controllers[jointIndex].rotationGoal = jointPosition;
+        controllers[jointIndex].robotState = state;
+    }
+
+
+
+
+}
